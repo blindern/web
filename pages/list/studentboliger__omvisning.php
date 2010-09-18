5603d5c3c098d7135f39c0544dc3a7609ec1868e
Henrik Steen <henrist@henrist.net>
2010-09-18 15:17:04 +0000

    Ny digital omvisning. Støtter opp mot søkemotorer og skal fungere på alle standardiserte enheter, inkludert mobiler etc.

--- pages/list/studentboliger__omvisning.php
+++ pages/list/studentboliger__omvisning.php
@@ -1,39 +1,334 @@
 <?php
 
+$images_raw = <<<IMAGELIST
+trapp_paere.jpg
+	Bunnen av pærealleen. T-banen ligger ca 70 meter til venstre for denne trappen. Foto: Cecilie Sæle Merkesvik
+
+etgodthjem.jpg
+	Skiltet med "Et godt hjem for studenter" kan sees på den ene stolpen i bunnen av pærealleen. Under dette mottoet ble studenterhjemmet bygget. Foto: Cecilie Sæle Merkesvik
+
+paere1.jpg
+	Den mektige pærealleen. Foto: Cecilie Sæle Merkesvik
+
+paere2.jpg
+	Pærealleen og østfløy. Det er på denne plenen sølecuppen arrangeres. Foto: Cecilie Sæle Merkesvik
+
+paere3.jpg
+	Østfløy sett fra pærealleen en vakker sommerdag. Foto: Cecilie Sæle Merkesvik
+
+hovedbyggvinter.jpg
+	Hovedbygget en kald vinterkveld. Foto: Simen Buodd
+
+hovedbygg1.jpg
+	Tre travle karer. Er det Onsdag, og er de på vei til Bunnpris som ligger rett over veien? Foto: Cecilie Sæle Merkesvik
+
+hovedbygg2.jpg
+	Bygget som er fra 1925 ble i 1997 fredet av riksantivkvaren. Foto: Cecilie Sæle Merkesvik
+
+sykkelkjeller2.jpg
+	I sykkelkjelleren kan man sette sykkelen om vinteren. Foto:Simen Buodd
+
+foaje.jpg
+	Helt inners ser man hoveddøren. Til høyre i foajeen er peisestuen, og til venstre er matsalen. Foto: Cecilie Sæle Merkesvik
+
+kollegievegg.jpg
+	Veggene på foajeen er dekorert med bilder av alle Kollegiene, helt tilbake til 1925. Foto: Simen Buodd
+
+posthyller.jpg
+	Her sorteres og legges posten hver morgen. Hvert rom har sin hylle. Foto: Simen Buodd
+
+bordtennis.jpg
+	Foreningen Blindern Studenterhjem og IFBS disponerer bl.a. et bordtennisbord som kan fritt benyttes av alle beboere. Foto: Simen Buodd
+
+tirill_solecup.jpg
+	Et av brukets mange tradisjoner er sølecup. Her er det ikke nødvendigvis den med flest mål som vinner. Foto: Tiril Stensgård Finnanger
+
+tennis.jpg
+	Studenterhjemmet disponerer også sin egen tennisbane. Denne kan fritt benyttes av alle brukets beboere, og innimellom arrangerer IFBS også egene tennisturneringer. Foto: Cecilie Sæle Merkesvik
+
+billa1.jpg
+	Når baren ikke er åpen, benyttes lokalet til hygge. Lokalet er åpent hele tiden, og billjardbordet er flittig i bruk på ettermiddagene. Foto: Cecilie Sæle Merkesvik
+
+badstue1.jpg
+	I kjelleren på hovedbygget er det en nyoppusset badstue som kan benyttes av alle brukets beboere. Dette er også tilholdsstedet for gutteforeningen BBB - Blindern Bad og Badstue. Foto: Simen Buodd
+
+moebelkjeller.jpg
+	Dersom du har egene møbler, kan disse lagres i møbelkjelleren over sommeren. Foto: Simen Buodd
+
+milla_valhal.jpg
+	Valhall er stedet for spontane fester, bursdager og annen hygge. Foto: Milla Melgaard
+
+matsal.jpg
+	I matsalen serveres det tre måltider hver dag. Veggene er prydet med tradisjonsrike skjold over beboere osm har utmerket seg oppigjennom årene. Foto: Simen Buodd
+
+musikksalong.jpg
+	Dette er brukets Musikksalong, det fineste møteromet. Her holder bl.a. Kollegiet sine ukentlige møter, og under spesielle begivenheter åpnes det bar er inne. Foto: Simen Buodd
+
+romellen1.jpg
+	Flittig student på et av rommene på Hovedbygget. Skrivebord, hylle, stol og seng er standardinventar og står klart på alle rom. Foto: Simen Buodd
+
+fotball.jpg
+	På brukets store tomt ligger det også en fotballbane og en sandvollyballbane. Foto: Cecilie Sæle Merkesvik
+
+romellen2.jpg
+	Det sies at fra enkelte av rommene på hovedbygget kan man se helt til Oslofjorden. 
+
+skjold.jpg
+	Beboere som utmerker seg spesielt i løpet av sin botid på Bruket, kan få et skjold på veggen. Her henger det skjold helt tilbake fra Brukets spede barndom. Foto: Simen Buodd
+
+peisestue.jpg
+	Erik og Johan slapper av i en av lesekrokene i peisestuen. Foto: Simen Buodd
+
+milla_hornoch.jpg
+	Blindern Haarn oc Blaese Orchester er brukets eget orkester, og spiller opp til både små og store anledninger. Foto: Milla Melgaard
+
+dassbilla.jpg
+	Doen i Billa-bar har meget spesiell innredning. Her kan man finne sangglade mennesker og synge alle slags sviser ut i de sene nattetimer. Foto: Simen Buodd
+
+tirill_hornoch.jpg
+	Blindern Haarn oc Blaese Orchester har statig oppdrag utenfor studenterhjemmet. Det anbefales på det sterkeste å bli med i en forening, og Haarn oc Blaese er intet dumt valg. Foto: Simen Buodd
+
+dusj1.jpg
+	For tiden holdes dusjene på å pusses opp. Slik ser en nyoppusset dusj ut. Foto: Simen Buodd
+
+rom1.jpg
+	Rommene er både store og lyse. Dette er et dobbeltrom med utgang til en egen balkong. Foto: Cecilie Sæle Merkesvik
+
+bad1.jpg
+	Nyoppusset bad fra 2009. Foto: Simen Buodd
+
+fagereng_glade.jpg
+	Det sosiale er meget viktig på Bruket, og det tar ikke lange tiden før nyinnflyttede glir inn i det gode miljøet på bruket. Foto: Anders Fagereng
+
+romellen3.jpg
+	Nok et enkelt innredet rom. Dette er et enkeltrom på hovedbygget. Foto: Simen Buodd
+
+dusj2.jpg
+	De fleste rommene deler dusj med naboen. Foto: Simen Buodd
+
+milla_advent.jpg
+	Pigefaareningen arrangerer av og til arrangementer som her, juleverksted. Foto: Milla Melgaard
+
+romerbad2.jpg
+	Brukets eget romerbad. Her arrangeres både årlig togafest, og annenhvert år omdannes dette til Ulka - brukets bruneste pub. Foto: Simen Buodd
+
+milla_cHorusBuchus.jpg
+	cHorus Buchus er brukets egene mannskor. Her under fremføringen av "Father Christmas" på julemøtet. Foto: Milla Melgaard
+
+vaskeri.jpg
+	Studentvaskeriet står åpent året rundt. Her er det bare å skrive seg opp på liste, så får man både vasket og tørket tøyet sitt. Foto: Cecilie Sæle Merkesvik
+
+matsal1.jpg
+	Kjøkkenet serverer variert og god, norsk mat. Det serveres tre måltider om dagen, der en av dem er varmmat. Foto: Cecilie Sæle Merkesvik
+
+gymsal.jpg
+	Gymsalen benyttes til alt fra yoga og vektløfting til konserter og diskotek. Salen står åpen hele døgnet, og kan benyttes av alle. Foto: Cecilie Sæle Merkesvik
+
+valhal1.jpg
+	Har du med deg glassflasker til Valhall, blir kvelden ekstra morsom etterhvert som flaskene blir tomme. Bank flasken tre ganger i bordet, og prøv å treffe blinken. Her nytter det ikke å feste uten sko! Foto: Cecilie Sæle Merkesvik
+
+peisestue1.jpg
+	Peisestua byr på et rolig lesemiljø på dagtid der mange velger å sitte å studere. Utover ettermiddagen overtar andre aktiviteter som brettspill, fyring i peisen og andre aktiviteter. Foto: Cecilie Sæle Merkesvik
+
+jan_vi.jpg
+	Jan IV er Brukets øverste beskytter. Foto: Cecilie Sæle Merkesvik
+
+balla.jpg
+	Balustraden på hovedbygget er et yppelig sted for kollokviegrupper, brettspill eller vors med gode venner. Foto: Cecilie Sæle Merkesvik
+
+bukkekollegiet.jpg
+	Bukkekollegiet er et av brukets eldste tradisjoner. Disse har ansvaret for å utnevne beboere som har vist en spesiel ånd for bruket til hel- eller halv-bukk slik at de får skjold på veggen i matsalen. Foto: Cecilie Sæle Merkesvik
+
+dagligeledere.jpg
+	På veggen opp til 2. etg henger bilder av alle Brukets tidligere daglige ledere. Foto: Cecilie Sæle Merkesvik
+
+valhall.jpg
+	Døren inn til festrommet Valhall. Foto: Cecilie Sæle Merkesvik
+
+lysekrone.jpg
+	Bygget er fredet, og derfor er også stilen bevart på inventar. Lysekroner slik som denne finnes det mange av. Foto: Cecilie Sæle Merkesvik
+
+billa.jpg
+	Billa-Bar, Brukets egen bar. Bar med alle rettigheter, holder åpent hver onsdag, og ca en Lørdag i måneden. Her holdes både større og mindre fester, og festforeningen har ansvaret for driften. Foto: Cecilie Sæle Merkesvik
+IMAGELIST;
+
+// parse images
+$images = array();
+$lines = explode("\n", $images_raw);
+$lines[] = "dummy"; // for å lagre siste bilde
+$file = null; $desc = null;
+foreach ($lines as $line)
+{
+	if (substr($line, 0, 1) == "\t")
+	{
+		if (!$file) continue;
+		$desc .= ($desc ? "\n" : "") . trim($line);
+	}
+	
+	else
+	{
+		if ($file)
+		{
+			$images[] = array(
+				$file,
+				$desc
+			);
+		}
+		
+		$file = trim($line);
+		$desc = null;
+	}
+}
+
+
 bs_side::$head .= '
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
-		<script type="text/javascript">
-		var flashvars = {};
-		flashvars.baseURL="'.bs_side::$pagedata->doc_path.'/simpleviewer/";
-		flashvars.galleryURL = "gallery.xml";
-		var params = {};
-		params.bgcolor = "222222";
-		params.allowfullscreen = true;
-		params.allowscriptaccess = "always";
-		swfobject.embedSWF("'.bs_side::$pagedata->doc_path.'/simpleviewer/simpleviewer.swf", "flashContent", "100%", "650", "9.0.124", false, flashvars, params);
-		</script>';
+<script src="/lib/mootools/mootools-1.2.x-core-nc.js" type="text/javascript"></script>
+<!--<script src="http://ajax.googleapis.com/ajax/libs/mootools/1.2.4/mootools-yui-compressed.js"></script>-->
+<script src="/lib/mootools/mootools-1.2.x-more-nc.js" type="text/javascript"></script>
+<script src="'.bs_side::$pagedata->doc_path.'/default.js"></script>
+<script>
+window.addEvent("domready", function()
+{
+	active_img = null;
+	load_hm();
+	var images = {};
+	
+	window.HM.addEvent("img-changed", function(data) {
+		set_img(data);
+	});
+	window.HM.addEvent("img-removed", function() {
+		set_img(null);
+	});
+	
+	$$("#omvisning_bilder p").addEvent("click", function(e)
+	{
+		window.HM.set("img", this.get("id").substring(4));
+		e.stop();
+	});
+	$("omvisning_back").addEvent("click", function(){ window.HM.remove("img"); }).addEvent("mousedown", function(e){e.stop()});
+	$("omvisning_prev").addEvent("click", function(){ rotate_img(true); }).addEvent("mousedown", function(e){e.stop()});
+	$("omvisning_next").addEvent("click", function(){ rotate_img(); }).addEvent("mousedown", function(e){e.stop()});
+	$("omvisning_bilde").addEvent("click", function(){ rotate_img(); });
+	
+	window.HM.recheck();
+	
+	function set_img(id)
+	{
+		var pa = active_img;
+		var p = active_img = $("img_"+id);
+		if (!p)
+		{
+			$("omvisning_bilde_inactive").setStyle("display", "");
+			$("omvisning_bilde_w").addClass("omvisning_bilde_skjult");
+			$("contentContainer").removeClass("contentFullsize");
+			if (id) window.HM.remove("img");
+			
+			if (pa) pa.goto(-30);
+			
+			return;
+		}
+		
+		$$("#omvisning_bilder p").removeClass("omvisning_aktiv");
+		$("omvisning_bilde_inactive").setStyle("display", "none");
+		$("contentContainer").addClass("contentFullsize");
+		
+		var c = $("omvisning_bilde");
+		c.empty().getParent().removeClass("omvisning_bilde_skjult");
+		
+		prepare(p).inject(c);
+		new Element("p", {text: p.getElement("img").get("alt")}).inject(c);
+		//c.getParent().goto(-10);
+		
+		// sett thumbnail som aktivt
+		p.addClass("omvisning_aktiv");
+	}
+	
+	window.addEvent("keydown", function(event)
+	{
+		// 37: left, 39: right
+		if (event.code == 37 || event.code == 39)
+		{
+			var t = event.target.get("tag");
+			if (t != "html" && t != "body") return;
+			rotate_img(event.code == 37, event);
+		}
+	});
+	
+	function rotate_img(prev, event)
+	{
+		// har vi bilde som referanse?
+		if (!active_img) return;
+		
+		// hent bildet vi skal gå til
+		var to = get_upcoming(active_img, prev);
+		
+		// last inn neste bilde vi forventer at blir vist
+		prepare(get_upcoming(to, prev));
+		
+		window.HM.set("img", to.get("id").substring(4));
+	}
+	
+	function get_upcoming(elm, prev)
+	{
+		var to = prev ? elm.getPrevious("p") : elm.getNext("p");
+		if (!to)
+		{
+			if (prev) to = $("omvisning_bilder").getLast("p");
+			else to = $("omvisning_bilder").getFirst("p");
+		}
+		
+		return to;
+	}
+	
+	function prepare(p)
+	{
+		var id = p.get("id").substring(4);
+		if (!images[id])
+		{
+			images[id] = new Element("img");
+			images[id].set("src", p.getElement("a").get("href"));
+		}
+		
+		return images[id];
+	}
+});
+</script>';
 
-?>
-                            <!--
-							<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="640" height="480" id="omvisning" align="middle">
-                              <param name="allowScriptAccess" value="sameDomain" />
-                              <param name="movie" value="omvisning.swf" />
 
-                              <param name="quality" value="high" />
-                              <param name="bgcolor" value="#ffffff" />
-                              <embed src="omvisning.swf" quality="high" bgcolor="#ffffff" width="640" height="480" name="omvisning" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
-                            </object>
-                            -->
-                            <h1>Digital Omvisning</h1>
-                            <h2 style="margin-bottom: 10px">Blider fra Blinderen Studenterhjem</h2>
-							<div id="flashContent">SimpleViewer requires JavaScript and the Flash Player.
-							<a href="http://www.adobe.com/go/getflashplayer/">Get Flash.</a></div><br>
-                            <h2 style="margin-top: 20px">Media om Blindern Studenterhjem: </h2>
-                            <p>
-                            	Blindern Studenterhjem ble k&aring;ret av TV-Norge
-                            	til beste studentbolig i Oslo.<br>
-							</p>
-                            <object width="425" height="350">
-                              <param name="movie" value="http://www.youtube.com/v/0q4U6N6Qsd4"></param>
-                              <embed src="http://www.youtube.com/v/0q4U6N6Qsd4" type="application/x-shockwave-flash" width="425" height="350"></embed>
-							</object>
+
+echo '
+<h1>Digital Omvisning</h1>
+<h2>Blider fra Blinderen Studenterhjem</h2>
+<div id="omvisning_bilde_w" class="omvisning_bilde_skjult">
+	<p id="omvisning_nav">
+		<span id="omvisning_back"><span>Tilbake til oversikt</span></span>
+		<span id="omvisning_prev"><span>Forrige bilde</span></span>
+		<span id="omvisning_next"><span>Neste bilde</span></span><br />(Piltaster kan også benyttes.)
+	</p>
+	<p id="omvisning_bilde"></p>
+</div>
+<div id="omvisning_bilde_inactive">
+	<p id="omvisning_bilder_h">Trykk på et bilde for å vise stort bilde.</p>
+	<div id="omvisning_bilder">';
+
+foreach ($images as $image)
+{
+	$key = substr(md5($image[0]), 0, 7);
+	
+	echo '
+		<p id="img_'.$key.'">
+			<a href="'.bs_side::$pagedata->doc_path.'/graphics/images/slideshow/images/'.htmlspecialchars($image[0]).'">
+				<img src="'.bs_side::$pagedata->doc_path.'/graphics/images/slideshow/thumbnails/'.htmlspecialchars($image[0]).'" alt="'.htmlspecialchars($image[1]).'" />
+			</a>
+		</p>';
+}
+
+echo '
+	</div>
+	<h2>Media om Blindern Studenterhjem: </h2>
+	<p>Blindern Studenterhjem ble k&aring;ret av TV-Norge til beste studentbolig i Oslo.</p>
+	<object width="425" height="350">
+		<param name="movie" value="http://www.youtube.com/v/0q4U6N6Qsd4"></param>
+		<embed src="http://www.youtube.com/v/0q4U6N6Qsd4" type="application/x-shockwave-flash" width="425" height="350"></embed>
+	</object>
+</div>';
