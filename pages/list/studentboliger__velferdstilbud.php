ce9d504f7abb246bdfa808e15da1f2e8b7607b10
Henrik Steen <henrist@henrist.net>
2010-10-06 20:52:06 +0000

    Velferdstilbudene i listeform i stedet for under egne overskrifter.

--- pages/list/studentboliger__velferdstilbud.php
+++ pages/list/studentboliger__velferdstilbud.php
@@ -7,63 +7,74 @@
 								p&aring; Blindern Studenterhjem. En kort beskrivelse
 								av noen av dem kan du finne her.
 							</p>
-
-							<h2>Egen pub og festlokaler</h2>
-							<p>
+							
+							<ul>
+								<li><b>Egen pub og festlokaler</b> -
 								Blindern Studenterhjem har egen pub, "Billa",
 								med alle rettigheter. "Billa" &aring;pnes
 								hver onsdag, og er i helgene ogs&aring; &aring;sted
-								for de fleste av Studenterhjemmets mange fester.
-							</p>
-
-							<h2>Hytte i B&aelig;rumsmarka</h2>
-							<p>
+								for de fleste av Studenterhjemmets mange fester.</li>
+								
+								<li><b><a href="../smaabruket">Hytte i Bærumsmarka</a></b> -
 								Blindern Studenterhjem eier en stor hytte i
 								B&aelig;rumsmarka til fri benyttelse for beboerne.
 								Studenter som skal p&aring; hyttetur kan f&aring;
-								sendt med mat fra kj&oslash;kkenet.
-							</p>
-							<h2>Vaskeri</h2>
-							<p>
+								sendt med mat fra kj&oslash;kkenet.</li>
+								
+								<li><b>Vaskeri</b> -
 								P&aring; Blindern Studenterhjem finnes et eget studentvaskeri,
 								som beboere kan benytte fritt uten &aring; betale
-								ekstra
-							</p>
-							<img src="<?php echo bs_side::$pagedata->doc_path; ?>/graphics/images/lesesalen_2.jpg" class="img_right">
-							<h2>Bibliotek og lesesal</h2>
-							<p>
+								ekstra</li>
+							</ul>
+							
+							<img src="<?php echo bs_side::$pagedata->doc_path; ?>/graphics/images/lesesalen_2.jpg" class="img_right" />
+							<ul>
+								
+								<li><b>Bibliotek og lesesal</b> -
 								P&aring; Studenterhjemmets bibliotek er det femti
-								lesesalsplasser til fri benyttelse.
-							</p>
-
-							<h2>Kollokvierom</h2>
-							<p>
+								lesesalsplasser til fri benyttelse.</li>
+								
+								<li><b>Kollokvierom</b> -
 								Kollokvierommet i Studenterhjemmets kjeller kan benyttes
-								til seminarer og gruppearbeid.
-							</p>
-
-							<h2>Kopimaskinsrom</h2>
-							<p>Kopimaskinen kan benyttes fritt av alle beboere</p>
-
-							<h2>Peisestue med tr&aring;dl&oslash;st nettverk</h2>
-							<p>
+								til seminarer og gruppearbeid.</li>
+								
+								<li><b>Kopimaskin</b> -
+								Kopimaskinen kan benyttes fritt av alle beboere</li>
+								
+								<li><b>Peisestue med tr&aring;dl&oslash;st nettverk</b> -
 								I Studenterhjemmets gamle peisestue kan beboerne
 								drikke kaffe, kose seg foran peisen og anvende
-								det tr&aring;dl&oslash;se nettverket.
-							</p>
-							<h2>TV-Stue</h2>
-							<p>
+								det tr&aring;dl&oslash;se nettverket.</li>
+								
+								<li><b>TV-stue</b> -
 								TV-stuen p&aring; Studenterhjemmet har ViaSat- og
-								Canal Digital-abonnement.
-							</p>
-							<img src="<?php echo bs_side::$pagedata->doc_path; ?>/graphics/images/peisestuen.jpg" class="img_right">
-							<h2>Flere musikkrom</h2>
-							<p>
+								Canal Digital-abonnement.</li>
+							</ul>
+							
+							<img src="<?php echo bs_side::$pagedata->doc_path; ?>/graphics/images/peisestuen.jpg" class="img_right" />
+							<ul>
+								<li><b>Flere musikkrom</b> -
 								Det finnes flere musikkrom p&aring; Blindern Studenterhjem,
 								blant annet en musikksalong med flygel, et &oslash;vingsrom
 								beregnet p&aring; band, og et musikkrom egnet for
-								orkester og kor.
-							</p>
+								orkester og kor.</li>
+								
+								<li><b>Gymsal og styrkerom</b> -
+								I den nyoppussede gymsalen kan man drive med styrketrening,
+								innebandy, bordtennis og andre aktiviteter.</li>
+								
+								<li><b>Sandvolleyballbane, fotballbane og tennisbane</b> -
+								P&aring; utearealene finnes en sandvolleyballbane
+								som er i flittig bruk gjennom &aring;rets varmere
+								halvdel. Like ved sandvolleyballbanen finnes Studenterhjemmets
+								fotballbane og to tennisbaner.</li>
+								
+								<li><b>Hageanlegget</b> -
+								I Studenterhjemmets velholdte hageanlegg kan man slappe
+								av mellom lese&oslash;ktene eller ha kollokvier p&aring;
+								gresset, eller bare nyte noen timer i solen.</li>
+							</ul>
+							
 							<h2>UKA p&aring; Blindern</h2>
 							<p>
 								Annenhvert &aring;r arrangeres UKA p&aring; Blindern
@@ -74,22 +85,4 @@
 								for beboere og eksterne bes&oslash;kende. Bes&oslash;k
 								<a href="http://www.blindernuka.no" target="_blank">www.blindernuka.no</a>
 								for mer informasjon.
-							</p>
-							<h2>Gymsal og styrkerom</h2>
-							<p>
-								I den nyoppussede gymsalen kan man drive med styrketrening,
-								innebandy, bordtennis og andre aktiviteter.
-							</p>
-							<h2>Sandvolleyballbane, fotballbane og tennisbane</h2>
-							<p>
-								P&aring; utearealene finnes en sandvolleyballbane
-								som er i flittig bruk gjennom &aring;rets varmere
-								halvdel. Like ved sandvolleyballbanen finnes Studenterhjemmets
-								fotballbane og to tennisbaner.
-							</p>
-							<h2>Hageanlegget</h2>
-							<p>
-								I Studenterhjemmets velholdte hageanlegg kan man slappe
-								av mellom lese&oslash;ktene eller ha kollokvier p&aring;
-								gresset, eller bare nyte noen timer i solen.
 							</p>
