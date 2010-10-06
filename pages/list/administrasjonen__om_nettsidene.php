ca2ec50c11e0cc232cd114d6caa2dfea1aee64bb
Henrik Steen <henrist@henrist.net>
2010-10-06 19:59:11 +0000

    Nettsiden er utformet i samarbeid med hele administrasjonen. Spesifisert at fotografene gjelder bilder ikke annet er nevnt.

--- pages/list/administrasjonen__om_nettsidene.php
+++ pages/list/administrasjonen__om_nettsidene.php
@@ -1,23 +1,23 @@
 <?php bs_side::set_title("Om nettsidene"); ?>
-                        <img src="<?php echo bs_side::$pagedata->doc_path; ?>/graphics/images/trapp_paerealle.jpg" class="img_right" />
+						<img src="<?php echo bs_side::$pagedata->doc_path; ?>/graphics/images/trapp_paerealle.jpg" class="img_right" />
 						<h1>Om nettsidene</h1>
-                        <p>
+						<p>
 							<strong>Henrik Steen</strong> har
 							designet og utformet denne nettsiden i samr&aring;d
-							med daglig leder <strong>Gro Osland</strong> og
+							med administrasjonen og
 							<strong>Kollegiet Buodd</strong> h&oslash;sten 2010.
-                            <strong>Inger Merete Hobbelstad</strong> har opprinnelig skrevet
-                            tekstene til nettsiden. <strong>Peter Holme</strong>
+							<strong>Inger Merete Hobbelstad</strong> har opprinnelig skrevet
+							tekstene til nettsiden. <strong>Peter Holme</strong>
 							og <strong>Cecilie S&aelig;le Merkesvik</strong> st&aring;r
-							bak fotografiene p&aring; nettsiden.
-                        </p>
-                        <p>
+							bak fotografiene p&aring; nettsiden hvor annet ikke er nevnt.
+						</p>
+						<p>
 							Dersom du lurer p&aring; noe ang&aring;ende
 							nettsidene s&aring; kan sp&oslash;rsm&aring;l
-                            rettes til hjemmesideoppmann ved Blindern Studenterhjem.
-                            For tiden er dette <strong>Henrik Steen</strong>.
-                        </p>
+							rettes til hjemmesideoppmann ved Blindern Studenterhjem.
+							For tiden er dette <strong>Henrik Steen</strong>.
+						</p>
 						<p>
-                            E-post adressen til hjemmesideoppmann er:<br />
+							E-postadressen til hjemmesideoppmann er:<br />
 							<a href="mailto:hjemmesideoppmann@blindern-studenterhjem.no">hjemmesideoppmann@blindern-studenterhjem.no</a>
 						</p>
