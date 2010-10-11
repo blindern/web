112a42ff1fce3836ee8a87980d995962528c02f2
Henrik Steen <henrist@henrist.net>
2010-10-11 10:17:59 +0000

    Ny liste over velferdstilbud. Kort og mer konkret.

--- pages/list/studentboliger__velferdstilbud.php
+++ pages/list/studentboliger__velferdstilbud.php
@@ -1,5 +1,7 @@
 <?php bs_side::set_title("Velferdstilbud"); ?>
 							<?php echo get_right_img("hyttenibaerumsmarka.jpg"); ?> <!-- Foto: -->
+							<?php echo get_right_img("lesesalen_2.jpg"); ?> <!-- Foto: -->
+							<?php echo get_right_img("peisestuen.jpg"); ?> <!-- Foto: -->
 							<h1>Felles velferdstilbud</h1>
 							<h2>Velferdstilbud</h2>
 							<p>
@@ -9,19 +11,49 @@
 							</p>
 							
 							<ul>
-								<li><b>Egen pub og festlokaler</b> -
+								<li>TV-stue</li>
+								<li><a href="omvisning#?img=c20785a">Egen pub og festlokaler</a></li>
+								<li><a href="../smaabruket">Hytte i Bærumsmarka</a></li>
+								<li><a href="omvisning#?img=2e683c1">Vaskeri</a></li>
+								<li>Bibliotek og lesesal</li>
+								<li>Kollokvierom</li>
+								<li>Kopimaskin</li>
+								<li>Telefon på alle rom</li>
+								<li><a href="omvisning#?img=5e9cd07">Peisestue med trådløst nettverk</a></li>
+								<li>Studentkjøkken med mulighet for bakst og egne måltider</li>
+								<li><a href="omvisning#?img=ea39fff">Badstue</a></li>
+								<li><a href="omvisning#?img=9caf503">Tennisbane</a></li>
+								<li><a href="omvisning#?img=60e5af3">Fotballbane</a></li>
+								<li><a href="omvisning#?img=60e5af3">Sandvolleyballbane</a></li>
+								<li><a href="omvisning#?img=9f5c52f">Bordtennisbord</a></li>
+								<li>Biljardbord</li>
+								<li>Musikkrom, musikksallong med flygel, bandrom</li>
+								<li><a href="omvisning#?img=e8dab86">Gymsal og styrkerom</a></li>
+								<li>Store grøntområder</li>
+								<li>Projektor</li>
+								<li>Mange forskjellige hagespill</li>
+								<li>En rekke brettspill</li>
+								<li><a href="omvisning#?img=f27adde">Sykkelkjeller</a></li>
+								<li><a href="omvisning#?img=5264ecf">Møbelkjeller</a></li>
+								<li>Skistall</li>
+								<li><a href="omvisning#?img=c8c2102">Festrom som tåler en støyt</a></li>
+							</ul>
+								
+							<?php /*
+							<ul>
+								<li><u>Egen pub og festlokaler</u> -
 								Blindern Studenterhjem har egen pub, "Billa",
 								med alle rettigheter. "Billa" &aring;pnes
 								hver onsdag, og er i helgene ogs&aring; &aring;sted
 								for de fleste av Studenterhjemmets mange fester.</li>
 								
-								<li><b><a href="../smaabruket">Hytte i Bærumsmarka</a></b> -
+								<li><u><a href="../smaabruket">Hytte i Bærumsmarka</a></u> -
 								Blindern Studenterhjem eier en stor hytte i
 								B&aelig;rumsmarka til fri benyttelse for beboerne.
 								Studenter som skal p&aring; hyttetur kan f&aring;
 								sendt med mat fra kj&oslash;kkenet.</li>
 								
-								<li><b>Vaskeri</b> -
+								<li><u>Vaskeri</u> -
 								P&aring; Blindern Studenterhjem finnes et eget studentvaskeri,
 								som beboere kan benytte fritt uten &aring; betale
 								ekstra</li>
@@ -30,50 +62,55 @@
 							<?php echo get_right_img("lesesalen_2.jpg"); ?> <!-- Foto: -->
 							<ul>
 								
-								<li><b>Bibliotek og lesesal</b> -
+								<li><u>Bibliotek og lesesal</u> -
 								P&aring; Studenterhjemmets bibliotek er det femti
 								lesesalsplasser til fri benyttelse.</li>
 								
-								<li><b>Kollokvierom</b> -
+								<li><u>Kollokvierom</u> -
 								Kollokvierommet i Studenterhjemmets kjeller kan benyttes
 								til seminarer og gruppearbeid.</li>
 								
-								<li><b>Kopimaskin</b> -
+								<li><u>Kopimaskin</u> -
 								Kopimaskinen kan benyttes fritt av alle beboere</li>
 								
-								<li><b>Peisestue med tr&aring;dl&oslash;st nettverk</b> -
+								<li><u>Peisestue med tr&aring;dl&oslash;st nettverk</u> -
 								I Studenterhjemmets gamle peisestue kan beboerne
 								drikke kaffe, kose seg foran peisen og anvende
 								det tr&aring;dl&oslash;se nettverket.</li>
 								
-								<li><b>TV-stue</b> -
+								<li><u>TV-stue</u> -
 								TV-stuen p&aring; Studenterhjemmet har ViaSat- og
 								Canal Digital-abonnement.</li>
+								
+								<li><u>Badstue</u> -
+								Stor og flott badstue er til fri disposisjon for beboere.</li>
 							</ul>
 							
 							<?php echo get_right_img("peisestuen.jpg"); ?> <!-- Foto: -->
 							<ul>
-								<li><b>Flere musikkrom</b> -
+								<li><u>Flere musikkrom</u> -
 								Det finnes flere musikkrom p&aring; Blindern Studenterhjem,
 								blant annet en musikksalong med flygel, et &oslash;vingsrom
 								beregnet p&aring; band, og et musikkrom egnet for
 								orkester og kor.</li>
 								
-								<li><b>Gymsal og styrkerom</b> -
+								<li><u>Gymsal og styrkerom</u> -
 								I den nyoppussede gymsalen kan man drive med styrketrening,
 								innebandy, bordtennis og andre aktiviteter.</li>
 								
-								<li><b>Sandvolleyballbane, fotballbane og tennisbane</b> -
+								<li><u>Sandvolleyballbane, fotballbane og tennisbane</u> -
 								P&aring; utearealene finnes en sandvolleyballbane
 								som er i flittig bruk gjennom &aring;rets varmere
 								halvdel. Like ved sandvolleyballbanen finnes Studenterhjemmets
 								fotballbane og to tennisbaner.</li>
 								
-								<li><b>Hageanlegget</b> -
+								<li><u>Hageanlegget</u> -
 								I Studenterhjemmets velholdte hageanlegg kan man slappe
 								av mellom lese&oslash;ktene eller ha kollokvier p&aring;
 								gresset, eller bare nyte noen timer i solen.</li>
 							</ul>
+							*/
+							?>
 							
 							<h2>UKA p&aring; Blindern</h2>
 							<p>
