99c43c326ca7cbc4d4d043b459477e80658f7b37
Henrik Steen <henrist@henrist.net>
2010-12-16 13:08:19 +0000

    Arrangementsplan for våren 2011. Også endret fra "arrangementplan" til å inneholde en s. Fikset blokkskrift i CSS ved søknader.

---
+++ pages/list/foreninger__arrangementsplan.php
@@ -0,0 +1,109 @@
+<?php bs_side::set_title("Arrangementplan"); ?>
+			<?php echo get_right_img("schussrenn_2010v.jpg", null, "Schüssrennet våren 2010", "Schüssrennet våren 2010. Foto: Anders Fagereng"); ?> <!-- Foto: Anders Fagereng -->
+			<h1>Arrangementer</h1>
+			<h2>Arrangementsplan for våren 2011</h2>
+			<dl class="arrangementer">
+				<dt>8. januar</dt>
+				<dd>Uoffisiell velkommen hjem-fest</dd>
+				
+				<dt>13. januar</dt>
+				<dd>Pygmémøte</dd>
+				
+				<dt>14. januar</dt>
+				<dd>Velkommen hjem-fest</dd>
+				
+				<dt>16. januar</dt>
+				<dd>Pygmemiddag m/cafe avec</dd>
+				
+				<dt>26. jan - 6. feb</dt>
+				<dd><b>UKA på Blindern 2011</b> - se <a href="http://blindernuka.no/">blindernuka.no</a></dd>
+				
+				<dt>5. februar</dt>
+				<dd>Holmenkollen skimaraton (IFBS)</dd>
+				
+				<dt>18. februar</dt>
+				<dd>Pygmétur til <a href="<?php echo bs_side::$pagedata->doc_path; ?>/smaabruket">Småbruket</a> (hytta)</dd>
+				
+				<dt>19. februar</dt>
+				<dd>Temafest</dd>
+				
+				<dt>23. feb - 6. mar</dt>
+				<dd>Ski-VM i Oslo (IFBS arrangerer camp)</dd>
+				
+				<dt>5. mars</dt>
+				<dd>Revyball m/cafe avec</dd>
+				
+				<dt>18. mars</dt>
+				<dd>Pigefaarsamlingens semestermøte og PPP</dd>
+				
+				<dt>19. mars</dt>
+				<dd>Birkebeinerrennet (IFBS)</dd>
+				
+				<dt>1.-3. april</dt>
+				<dd>Skitur til Hemsedal (IFBS)</dd>
+				
+				<dt>9. april</dt>
+				<dd>Scüssrenn</dd>
+				
+				<dt>22. april</dt>
+				<dd>Hyttedugnad til <a href="<?php echo bs_side::$pagedata->doc_path; ?>/smaabruket">Småbruket</a></dd>
+				
+				<dt>30. april</dt>
+				<dd>Russefest</dd>
+				
+				<dt>9. mai</dt>
+				<dd>Temafest</dd>
+				
+				<dt>17. mai</dt>
+				<dd>17. mai med cafe avec</dd>
+				
+				<dt>19. mai</dt>
+				<dd>Allmannamøte</dd>
+				
+				<dt>4. juni</dt>
+				<dd>Bukkehaugsfestivalen</dd>
+			</dl>
+			<p>Sist oppdatert 16. desember 2010.</p>
+			
+			<!--<h2>Arrangementsplan for h&oslash;sten 2010</h2>
+			<dl class="arrangementer">
+				<dt>21. august</dt>
+				<dd>Rebusløp og velkommen hjem fest</dd>
+				
+				<dt>29. august</dt>
+				<dd>Pygmemiddag m/cafe avec</dd>
+				
+				<dt>4. september</dt>
+				<dd>Pygmetur til Småbruket (hytta)</dd>
+				
+				<dt>11. september</dt>
+				<dd>Temafest (tema: eventyr)</dd>
+				
+				<dt>25. september</dt>
+				<dd>Sølecup</dd>
+				
+				<dt>1. oktober</dt>
+				<dd>Pigefaarsamlingens semestermøte og PPP</dd>
+				
+				<dt>16. oktober</dt>
+				<dd>Høstball</dd>
+				
+				<dt>17. oktober</dt>
+				<dd>Cafe avec</dd>
+				
+				<dt>6. november</dt>
+				<dd>Togafest</dd>
+				
+				<dt>12. november</dt>
+				<dd>Hyttedugnad</dd>
+				
+				<dt>20. november</dt>
+				<dd>Revysus</dd>
+				
+				<dt>27. november</dt>
+				<dd>Temafest</dd>
+				
+				<dt>3. desember</dt>
+				<dd>Julemøte</dd>
+			</dl>
+			<p>Sist oppdatert 14. oktober 2010.</p>-->
