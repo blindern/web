f688473e5978fac901d2e3c96f5db2c573dc2d76
Henrik Steen <henrist@henrist.net>
2011-10-18 04:48:15 +0200

    La inn tekst om matrikkelen.

---
+++ pages/list/om__matrikkel.php
@@ -0,0 +1,99 @@
+<?php
+
+bs_side::set_title("Matrikkel");
+
+echo '
+<h1>Matrikkel</h1>
+<p><i>Matrikkelen over beboere ved Blindern Studenterhjem</i></p>
+<p>Matrikkelen er navnet på de to beboerdatabasene til Blindern Studenterhjem. Den ene favner årene før krigen (1925-42), den andre årene etter og opp til 2000.
+Til sammen inneholder de i underkant av 6 400 oppføringer med navn, informasjon om studie, hjemsted, fødeår, innflyttingsdato, utflyttingsdato og andre anmerkninger.</p>
+<table class="table" style="margin-bottom: 3px">
+	<thead>
+		<tr>
+			<th>Navn</th>
+			<th>Studie</th>
+			<th>Hjemsted</th>
+			<th>Fødeår</th>
+			<th>Innflyttet</th>
+			<th>Utflyttet</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>laane, earl ludvig</td>
+			<td>medisin</td>
+			<td>sem</td>
+			<td>04.09.1909</td>
+			<td>01.09.1929</td>
+			<td>1937 1</td>
+		</tr>
+		<tr>
+			<td>langballe, fredrik</td>
+			<td>jus</td>
+			<td>moss</td>
+			<td>15.06.1909</td>
+			<td>26.06.1927</td>
+			<td>1928 1</td>
+		</tr>
+		<tr>
+			<td>langberg, christian bjelland</td>
+			<td>treider</td>
+			<td>stavanger</td>
+			<td>04.03.1916</td>
+			<td>01.09.1935</td>
+			<td>1936 1</td>
+		</tr>
+		<tr>
+			<td>lange, henrik finne</td>
+			<td>medisin</td>
+			<td>trondheim</td>
+			<td>15.04.1906</td>
+			<td>01.09.1927</td>
+			<td>1931 1</td>
+		</tr>
+		<tr>
+			<td>lange, jean emil</td>
+			<td>medisin</td>
+			<td>arendal</td>
+			<td>26.09.1905</td>
+			<td>28.08.1926</td>
+			<td>1928 1</td>
+		</tr>
+		<tr>
+			<td>lange, sven de</td>
+			<td>jus</td>
+			<td>bergen</td>
+			<td>31.10.1918</td>
+			<td>15.01.1939</td>
+			<td>1946 1</td>
+		</tr>
+		<tr>
+			<td>lange, tormod olav</td>
+			<td>jus</td>
+			<td>seterstöa st.</td>
+			<td>24.10.1907</td>
+			<td>11.01.1927</td>
+			<td>1931 1</td>
+		</tr>
+		<tr>
+			<td>langfeldt, nicolay kristian fredrik</td>
+			<td>jus</td>
+			<td>kristiansand</td>
+			<td>16.12.1909</td>
+			<td>17.09.1927</td>
+			<td>1928 2</td>
+		</tr>
+		<tr>
+			<td>langlo, egill olsen</td>
+			<td>jus</td>
+			<td>bergen</td>
+			<td>06.04.1912</td>
+			<td>04.09.1933</td>
+			<td>1937 1</td>
+		</tr>
+	</tbody>
+</table>
+<p style="margin-top: 0; font-size: 95%"><i>Utdrag fra matrikkelen 1925-1942. De ti første oppføringene på &laquo;L&raquo;.</i></p>
+<p>For øyeblikket er ikke matrikkelen allmennt tilgjengelig. Det er usikkerhet rundt personvern og hvem som skal gis tilgang på dataene.
+Derfor bes de som ønsker å søke opp personer ta kontakt med <a href="mailto:hagpet@gmail.com">Arkivar Peter Hagen</a>.</p>
+<p>Matrikkelen er produsert av Peter Hagen og ble ferdigstilt i 2011.</p>';
