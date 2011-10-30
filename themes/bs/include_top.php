d8871f543a3c033b69024597af60a0030e20c320
Henrik Steen <henrist@henrist.net>
2011-10-30 02:50:05 +0100

    La til themes-mappa fra blindernuka.no.

---
+++ themes/bs/include_top.php
@@ -0,0 +1,44 @@
+<?php
+
+header("Content-Type: text/html; charset=utf-8");
+
+// sett opp head
+ess::$b->page->head = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="Henrik Steen [henrist.net], Kristian Saksvik Munkvold [krsjan.com]" />
+<meta name="generator" content="hsw.no websystem" />
+<meta name="keywords" content="'.ess::$b->page->generate_keywords().'" />
+<meta name="description" content="'.ess::$b->page->description.'" />
+<base href="'.ess::$s['path'].'/" />
+<link rel="shortcut icon" href="'.ess::$s['rpath'].'/favicon.ico" />
+<link href="'.ess::$s['rpath'].'/blindernuka.css?'.@filemtime(dirname(dirname(dirname(__FILE__)))."/blindernuka.css").'" rel="stylesheet" type="text/css" />
+<!--[if lte IE 8]>
+<script src="'.ess::$s['rpath'].'/html5ie.js" type="text/javascript"></script>
+<![endif]-->
+<meta name="robots" content="index, follow" />
+<link href="'.ess::$s['rpath'].'/rss" rel="alternate" type="application/rss+xml" title="RSS 2.0" />
+<script type="text/javascript" src="'.ess::$s['rpath'].'/mootools.1.3.js"></script>
+<script type="text/javascript" src="'.ess::$s['rpath'].'/mootools-more.1.3.js"></script>
+<script type="text/javascript" src="'.ess::$s['rpath'].'/blindernuka.js?update='.@filemtime(dirname(dirname(dirname(__FILE__)))."/blindernuka.js").'"></script>
+<!--[if lte IE 7]>
+<link rel="stylesheet" type="text/css" href="'.ess::$s['rpath'].'/blindernuka-ie7.css" />
+<![endif]-->'.ess::$b->page->head;
+
+// sett opp nettleser "layout engine" til CSS
+$list = array(
+	"opera" => "presto",
+	"applewebkit" => "webkit",
+	"msie 8" => "trident6 trident",
+	"msie 7" => "trident5 trident",
+	"msie 6" => "trident4 trident",
+	"gecko" => "gecko"
+);
+$class_browser = 'unknown_engine';
+$browser = strtolower($_SERVER['HTTP_USER_AGENT']);
+foreach ($list as $key => $item)
+{
+	if (strpos($browser, $key) !== false)
+	{
+		$class_browser = $item;
+		break;
+	}
+}
