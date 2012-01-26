4354e1c4dcbcc7dc7f486667d3948b2a8635cedc
Henrik Steen <henrist@henrist.net>
2012-01-26 11:13:29 +0100

    Oppdaterte statutter per 16. nov (Foreningens statutter).

--- pages/list/beboer.php
+++ pages/list/beboer.php
@@ -40,7 +40,7 @@ echo '
 <h2 id="nyttig">Nyttige dokumenter/ressurser</h2>
 <ul>
 	<li><a href="/velkommen.pdf">Velkomsthefte for Blindern Studenterhjem</a> (PDF, revidert høst 2011)</li>
-	<li><a href="/dokumenter/Statutter_2011-11-14.pdf">Statutter for Blinderen Studenterhjem</a> (PDF, oppdatert 14. november 2011)</li>
+	<li><a href="/dokumenter/Statutter_2011-11-16.pdf">Statutter for Blinderen Studenterhjem</a> (PDF, oppdatert 16. november 2011)</li>
 	<li><a href="/foreninger/arrangementsplan">Arrangementsplan</a> (oppdateres fortløpende)</li>
 </ul>
 
