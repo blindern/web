63305612359f3b9aa6b5c816f757c78c97b7b7f9
Henrik Steen <henrist@henrist.net>
2013-08-22 13:13:38 +0200

    Side med kort info om avmelding fra internmailen.

---
+++ pages/list/internmail.php
@@ -0,0 +1,18 @@
+<?php
+
+bs_side::set_title("Internmail");
+bs_side::$head .= '
+<meta name="robots" content="noindex, nofollow" />';
+
+bs_side::$menu_active = "beboer";
+
+echo '
+<h1>Internmailen</h1>
+<p><b>Avmelding:</b></p>
+<ol>
+       <li>Send en e-post til <b>sympa@studorg.uio.no</b> med emnet:<br />
+               <code>unsubscribe blindernbeboere</code></li>
+       <li>Du vil motta en bekreftelse for at du ble avmeldt</li>
+       <li>Se <a href="http://www.uio.no/tjenester/it/e-post-kalender/e-postlister/abonnere/unsub.html">brukerveiledningen</a> for hjelp</li>
+</ol>
+<p>Mer informasjon om listen ligger i wikien:<br /><a href="https://blindern-studenterhjem.no/wiki/Internmailen">https://blindern-studenterhjem.no/wiki/Internmailen</a></p>';
