57ace4fffe63d1f7df7eea767cd243cb448d2e6a
Henrik Steen <henrist@henrist.net>
2013-09-17 13:36:24 +0200

    La til info om biblioteksutvalget tilsendt av Mats Julius.

--- pages/list/livet__biblioteksutvalget.php
+++ pages/list/livet__biblioteksutvalget.php
@@ -8,6 +8,7 @@ $foreninger->set_active("biblioteksutvalget");
 
 echo '
 <h1>Biblioteksutvalget</h1>
-<p>Informasjon kommer!</p>';
+<p>Biblioteksutvalget forvalter boksamlingen til Blindern Studenterhjem. Helt fra 1925 har studenthjemmet mottatt en kontinuerlig strøm av nye bøker. Biblioteksutvalget i ledelse av bibliotekaren sørger for at relevant og oppdatert litteratur er utstilt ryddig i biblioteket.</p>
+<p>Utvalget kommer også med bokanbefalinger, og de arrangerer jevnlig kulturelle arrangementer, for eksempel foredrag, teaterutflukter og museumsbesøk.</p>';
 
 $foreninger->gen_page();
