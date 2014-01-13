758787b4547029b1ce9f8936c7b98c98314e44fe
Henrik Steen <henrist@henrist.net>
2014-01-13 02:27:06 +0100

    Oppdaterte innhold om Katiba (gammel mail fra Gøran). Fixes #5

--- pages/list/livet__katiba.php
+++ pages/list/livet__katiba.php
@@ -9,6 +9,23 @@ $foreninger->set_active("katiba");
 echo '
 <h1>Katiba Wa Mbilikimo</h1>
 
-<p>Informasjon kommer!</p>';
+<p>Katiba Wa Mbilikimo er Swahili og betyr &quot;Orden av pygméer&quot;.</p>
+<p>I 1998 ble &laquo;Ordenen Katiba Wa Mbilikimo&raquo; stiftet. Dens formål er
+å fremme de nyinnflyttede pygméenes trivsel og samhold, samt å være et
+springbrett til videre foreningsvirksomhet på BS.</p>
+
+<h2>&laquo;Jakten&raquo;</h2>
+
+<p>Katiba arrangerer fester og kurs for sine medlemmer, det største
+arrangementet hvert semester for ordenen er &laquo;Jakten&raquo; (på folkemunne PPP) som
+går parallellt med Pigeforsamlingens semestermøte. Det deles ut fellingstillatelser
+på pigefaar og de dresskledde medlemmene samles i peisestuen for å spille poker og blackjack, nyte live
+musikk og eksotisk underholdning følger med.</p>
+
+<p>De vakre bunnyene som er til stede er der for å fremme trivsel og øke motivasjonen til hva som
+venter festdeltakerene nede i Billa Bar, de forventer å bli tatt godt vare på,
+og hvis du gjør det, behandler de deg som en ekte James Bond ved pokerbordet.</p>
+
+<p><i>Katiba - Av pygméer for pygméer</i></p>';
 
 $foreninger->gen_page();
