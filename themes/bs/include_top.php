e3beafce2c768227158c2b25b1f191d214335f2b
Henrik Steen <henrist@henrist.net>
2014-07-14 20:34:53 +0200

    Revert "Legger til mobilscript fra 1881."
    This reverts commit 1256f4a9bed75f0e75d285de08e4d4badf878838.

--- themes/bs/include_top.php
+++ themes/bs/include_top.php
@@ -16,15 +16,7 @@ ess::$b->page->head = '<meta http-equiv="Content-Type" content="text/html; chars
 <link href="'.ess::$s['rpath'].'/layout/layout.css?'.@filemtime(dirname(dirname(dirname(__FILE__)))."/layout/layout.css").'" rel="stylesheet" type="text/css" />
 <!--[if lte IE 8]>
 <script src="'.ess::$s['rpath'].'/html5ie.js" type="text/javascript"></script>
-<![endif]-->
-<script type="text/javascript">
-var mobile_domain ="blindern-studenterhjem.1881gomo4u.com";
-// Set to false to not redirect on iPad.
-var ipad = false;
-// Set to false to not redirect on other tablets (Android , BlackBerry, WebOS tablets).
-var other_tablets = false;
-document.write(unescape("%3Cscript src=\'"+location.protocol+"//s3.amazonaws.com/me.static/js/me.redirect.min.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-</script>'.$robots.'
+<![endif]-->'.$robots.'
 '.ess::$b->page->head;
 
 // sett opp nettleser "layout engine" til CSS
