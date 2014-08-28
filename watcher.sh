adb21d4bfc11cb3d50fcb7594e7dcd87f14cdba2
Henrik Steen <henrist@henrist.net>
2014-08-28 20:50:08 +0200

    Krev at ny CSS fil blir lagret når man kjører watcher.

--- watcher.sh
+++ watcher.sh
@@ -5,4 +5,5 @@
 # Compiles CSS-files from SCSS-files
 #
 # to install: gem install sass
+sass --scss --sourcemap --style compressed --force --update layout/scss:layout
 sass --scss --sourcemap --style compressed --watch layout/scss:layout
